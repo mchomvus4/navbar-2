5d275d15a1d4d2910f775734c2dc51da0d739721
samwelnm4@gmail.com <samwelnm4@gmail.com>
2021-02-06 10:16:08 +0300

    navbar

---
+++ src/components/NAV/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Nav.css'
+const Navbar = () => {
+  return (
+    <div className="nav_header">
+    <div className="logo">
+    NAVBAR
+    </div>
+    <ul className="nav_ul">
+     <li>Home</li>
+     <li>About Us</li>
+     <li>Projects</li>
+     <li>Sign In</li>
+     <li>Sign Up</li>
+    </ul>
+    </div>
+  )
+}
+
+export default Navbar
