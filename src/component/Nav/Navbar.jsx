174861965466201cc9a8c9af718b2ebef5a4d27b
samwelnm4@gmail.com <samwelnm4@gmail.com>
2021-02-07 17:55:40 +0300

    logo

--- src/component/Nav/Navbar.jsx
+++ src/component/Nav/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import Burger from './Burger'
+import logo from '../../images/logo.png'
 
 const Nav = styled.nav`
  width: 100%;
@@ -9,16 +10,14 @@ const Nav = styled.nav`
  padding: 0 2rem;
  display: flex;
  justify-content: space-between;
- .logo{
-  padding: 1.5rem 0;
- }
+ 
 
 `
 const Navbar = () => {
   return (
    <Nav>
     <div className="logo"> 
-       NAVBAR
+       <img src={logo} alt="logo"/>
     </div>
     <Burger/>
     </Nav>
